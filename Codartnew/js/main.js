9b0bf5ea3d380f1cfa4503931e4afffdccc13921
Alex <Lev>
2021-04-08 14:50:55 +0300

    added codartnew

---
+++ Codartnew/js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('.mainscreen__slide').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        vertical: true,
+        verticalSwiping: true,
+        dots: true,
+        arrows: true,
+      });
+});
+$('.hamburger').click(function () {
+    $(this).toggleClass("hamburger_active");
+});
