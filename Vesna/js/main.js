a1c755d75d109da1b794ab9d0ce949b9b7cb4622
Alex <Lev>
2021-04-29 19:57:13 +0300

    added vesna full page without adaptive plus carto4ka

--- Vesna/js/main.js
+++ Vesna/js/main.js
@@ -1,8 +1,11 @@
 $(function(){
-    const swiper = new Swiper('.swiper-container', {
+     swiper = new Swiper('.swiper-container', {
         centeredSlides: true,
         slidesPerView: 1,
         mousewheel: true,
+        loop: true,
+        observer: true,
+        observeParents: true,
         pagination: {
           el: '.swiper-pagination',
           clickable: true,
@@ -12,7 +15,6 @@ $(function(){
           prevEl: '.swiper-button-prev',
         },
       });
-
 
       function toggleSlide(item) {
         $(item).each(function (i) {
@@ -32,13 +34,26 @@ $(function(){
 $('.info__form-item-guest').on('click', function(){
   $('.info__popup').slideToggle('2000',"swing");
 });
+$('.info__form-item-popup').on('click', function(){
+  $('.info__popup-modal').slideToggle('2000',"swing");
+});
 $('[data-modal=rules]').on('click', function () {
   $('.overlay, #rules').fadeIn('slow');
 });
+$('[data-modal=number-one]').on('click', function () {
+  $('.overlay, #number-one').fadeIn('slow');
+});
+$('[data-modal=number-popup]').on('click', function () {
+  $('.overlay, #number-popup').fadeIn('slow');
+});
 
 $('.rules__close').on('click', function () {
-  $('.overlay, #rules, #order, #thanks').fadeOut('slow');
+  $('.overlay, #rules, #number-one, #thanks, #number-popup').fadeOut('slow');
 });
+$('.number__popup-close').on('click', function () {
+  $('#number-popup').fadeOut('slow');
+});
+
 /* $('[data-modal=rules]').click(function () {
   $('.overlay').fadeIn();
 })
