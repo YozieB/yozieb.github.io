3b51b0330f78eed15b8265199e5c5e10ba720955
Alex <Lev>
2021-05-04 19:32:01 +0300

    added lamp prefinal

--- lamp/js/main.js
+++ lamp/js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-    const classHeader = 'questions_active';
+/*     const classHeader = 'questions_active';
     const classBody   = 'questions_content';
     const classActive = 'questions_content_active';
     
@@ -22,12 +22,54 @@ $(function(){
           closeAll(items, item);
         }
       });
-    });
-    
+    }); */
+    function toggleSlide(item) {
+      $(item).each(function (i) {
+        $(this).on('click', function (e) {
+          e.preventDefault();
+          $('.questions_content').eq(i).slideToggle();
+          $('.questions_open').eq(i).toggleClass('questions_open-rotated');
+        });
+      });
+    };
+    toggleSlide('.questions_accordion');
     const burgerButton = document.querySelector('.burger_button');
     const burgerList = document.querySelector('.burger_list');
     
     burgerButton.addEventListener('click', () => {
         burgerList.classList.toggle('burger_list_toggle');
     })
+});
+
+    SmoothScroll({  // Scrolling Core
+      animationTime    : 400, // [ms]
+      stepSize         : 100, // [px]
+  
+      // Acceleration
+      accelerationDelta : 50,  // 50
+      accelerationMax   : 3,   // 3
+  
+      // Keyboard Settings
+      keyboardSupport   : true,  // option
+      arrowScroll       : 50,    // [px]
+  
+      // Pulse (less tweakable)
+      // ratio of "tail" to "acceleration"
+      pulseAlgorithm   : true,
+      pulseScale       : 4,
+      pulseNormalize   : 1,
+  
+      // Other
+      touchpadSupport   : false, // ignore touchpad by default
+      fixedBackground   : true, 
+      excluded          : ''    });
+
+
+$("a[href^='#']").click(function(){
+  var _href = $(this).attr("href");
+  $("html, body").animate({scrollTop: $(_href).offset().top}, {
+    duration: 650,   // по умолчанию «400» 
+    easing: "swing" // по умолчанию «swing»
+  });
+  return false;
 });
