8badec11d6c84fc4be8499f04cc79b031c44fdb4
Alex <Lev>
2021-05-25 00:23:52 +0300

    added zoo finished

--- zoo/js/main.js
+++ zoo/js/main.js
@@ -1,8 +1,19 @@
 $(function(){
 
-    swiper = new Swiper('.swiper-container', {
+   const swiper = new Swiper('.swiper-container', {
         centeredSlides: true,
         slidesPerView: 5,
+        mousewheel: true,
+        loop: true,
+        spaceBetween: 80,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+      const swiper2 = new Swiper('.swiper-card', {
+        centeredSlides: true,
+        slidesPerView: 1,
         mousewheel: true,
         loop: true,
         spaceBetween: 80,
